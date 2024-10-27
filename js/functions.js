9d01031bb0428b3705ba33af24969466fba65a87
Александр Соловьев <sachasolo101@gmail.com>
2024-10-27 22:50:57 +0500

    подключение скрипта

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,24 @@
-function isStringLengthValid (stringInput, maxLength){
-  return stringInput <= maxLength;
+function isStringLengthValid(inputString, maxLength) {
+  return inputString.length <= maxLength;
 }
 
 console.log(isStringLengthValid('проверяемая строка', 20)); // true
 console.log(isStringLengthValid('проверяемая строка', 18)); // true
 console.log(isStringLengthValid('проверяемая строка', 10)); // false
+
+
+function isPalindrome(inputString) {
+  const normalizedString = inputString.replaceAll(' ', '').toLowerCase();
+  let reversedString = '';
+
+  for (let i = normalizedString.length - 1; i >= 0; i--) {
+    reversedString += normalizedString[i];
+  }
+
+  return normalizedString === reversedString;
+}
+
+console.log(isPalindrome('топот')); // true
+console.log(isPalindrome('ДовОд')); // true
+console.log(isPalindrome('Кекс')); // false
+console.log(isPalindrome('Лёша на полке клопа нашёл')); // true
