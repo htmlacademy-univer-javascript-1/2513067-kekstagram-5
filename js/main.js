a0f4abd95efbafef1ac9ec8ac7c466d4fce6f8e3
Александр Соловьев <sachasolo101@gmail.com>
2025-01-04 22:57:46 +0500

    переделка

--- js/main.js
+++ js/main.js
@@ -2,55 +2,42 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function getRandomElement(array) {
-  return array[getRandomInt(0, array.length - 1)];
+function getRandomElement(arr) {
+  return arr[getRandomInt(0, arr.length - 1)];
 }
 
-const descriptions = [
-  'Прекрасный вид!',
-  'Невероятная красота природы.',
-  'Лучший момент дня.',
-  'Воспоминания на всю жизнь.',
-  'Удивительное приключение.'
-];
+function generateComment(id) {
+  const messages = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  ];
+  const names = ['Артём", "Иван", "Мария", "Елена", "Ольга", "Дмитрий", "Алексей", "Сергей", "Анна", "Наталья'];
 
-const messages = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-
-const names = ['Артём', 'Иван', 'Ольга', 'Мария', 'Дмитрий', 'Елена'];
-
-function generateComments() {
-  const comments = [];
-  const commentCount = getRandomInt(0, 30);
-  for (let i = 0; i < commentCount; i++) {
-    comments.push({
-      id: getRandomInt(1, 1000),
-      avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
-      message: getRandomElement(messages),
-      name: getRandomElement(names)
-    });
-  }
-  return comments;
+  return {
+    id: id,
+    avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
+    message: getRandomElement(messages),
+    name: getRandomElement(names)
+  };
 }
 
-function generatePhotos() {
-  const photos = [];
-  for (let i = 1; i <= 25; i++) {
-    photos.push({
-      id: i,
-      url: `photos/${i}.jpg`,
-      description: getRandomElement(descriptions),
-      likes: getRandomInt(15, 200),
-      comments: generateComments()
-    });
-  }
-  return photos;
+function generatePhotoDescription(id) {
+  const commentsCount = getRandomInt(0, 30);
+  const comments = Array.from({ length: commentsCount }, (_, index) => generateComment(index));
+
+  return {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: `Описание фотографии ${id}`,
+    likes: getRandomInt(15, 200),
+    comments: comments
+  };
 }
 
-const photosArray = generatePhotos();
+const photos = Array.from({ length: 25 }, (_, index) => generatePhotoDescription(index + 1));
+photos();
+
