d54f5b6ee7d31256157b90b5e48e204ca62f473c
Александр Соловьев <sachasolo101@gmail.com>
2025-01-04 23:10:05 +0500

    5 задание

--- js/util.js
+++ js/util.js
@@ -1,7 +1,8 @@
+// util.js
 export function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export function getRandomElement(array) {
-  return array[getRandomInt(0, array.length - 1)];
+export function getRandomElement(arr) {
+  return arr[getRandomInt(0, arr.length - 1)];
 }
